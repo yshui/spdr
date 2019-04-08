62bb3602163658402c38d39377d9444f7b41cd2d
Your Name <you@example.com>
2019-04-08 21:18:29 +0100

    tmp

--- spdr/core/builder/internal.d
+++
@@ -1,8 +0,0 @@
-module spdr.core.builder.internal;
-import spdr.core.base;
-
-/// Basic internal builder
-/// Can build any set of dependency vertex
-bool build(ref State s) {
-	return true;
-}
--- spdr/core/file.d
+++ spdr/core/file.d
@@ -1,55 +1,43 @@
 module spdr.core.file;
 import spdr.core.base;
+import std.typecons;
 
 ///
 struct File {
+	/// Name of the file
+	string filename;
 	/// Whether the file exists
 	bool exist;
 }
 
 /// Represents a file that can be used as dependency
-final class DepFile : DepValue!File {
-private:
-	File result;
-	DepValue!string file_name;
+final class DepFile : DepImpure!(File, string) {
 protected:
-	override ref const(File) get_unchecked() const {
-		return result;
+	override Tuple!(Hash, File) nonrecursive_resolve(ref State s, string filename) {
+		Hash tmp_hash;
+		File result;
+		result.filename = filename;
+		s.fs.fetch(filename, tmp_hash, result.exist);
+		return tuple(tmp_hash, result);
 	}
-public:
-	override void resolve(ref State s) {
-		auto fname = file_name.get;
-		import std.file : exists;
-		import std.digest : makeDigest;
-		import std.digest.sha : SHA256;
-		result.exist = fname.exists;
-		auto sha = makeDigest!SHA256;
-		sha.put(cast(ubyte)result.exist);
-		if (result.exist) {
-			import std.stdio : File;
-			import std.digest : makeDigest;
-			import std.digest.sha : SHA256;
-			auto inf = File(fname);
-			foreach (ubyte[] buf; inf.byChunk(4096)) {
-				sha.put(buf);
-			}
-		}
-		set_hash(s, sha.finish());
-	}
-
-	///
-	this(DepValue!string fn) {
-		file_name = fn;
-		fn.add_dependent(this);
-		add_dependency(fn);
-	}
+	mixin DepCtor!(string);
 }
 
 /// Represent the file listing inside a directory
-final class DepDirEntries : DepValue!(string[]) {
+version(none) final class DepDirEntries : DepValue!(string[]) {
 private:
 	string[] result;
 	DepValue!string path;
 public:
 
+}
+
+unittest {
+	import std.stdio : writeln;
+	auto exename = depConst("/usr/bin/ls");
+	auto exe = new DepFile(exename);
+	State s;
+	auto v = exe.resolve(s);
+	writeln(v);
+	writeln(exe.resolve(s));
 }
